a78ad290089a219f3f9b9272403d6ffb89d01a01
Facu Rojo <facundoivanrojo@gmail.com>
2023-02-22 09:23:01 -0300

    add to implements and services

--- src/main/java/org/example/service/UserServiceImpl.java
+++ src/main/java/org/example/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package org.example.service;
 
 import org.example.dto.UserDto;
 import org.example.entitys.User;
+import org.example.exceptions.EmailAlreadyExistsException;
 import org.example.repositorys.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -9,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -31,7 +33,14 @@ public class UserServiceImpl implements UserDetailsService, UserService {
         if (user == null) {
             throw new UsernameNotFoundException("invalid username");
         }
-        return org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(), user.getTypeUser());
+        return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(), getAuthority(user));
+    }
+
+    private Set<SimpleGrantedAuthority> getAuthority(User user) {
+        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority(user.getTypeUser().name()));
+
+        return authorities;
     }
 
     @Override
@@ -44,12 +53,15 @@ public class UserServiceImpl implements UserDetailsService, UserService {
     @Override
     public User save(UserDto user) {
         User nUser = user.getUserFromDto();
-        if (userRepository.existsByEmail(nUser.getUserEmail())) ;
-        return null;
+        if (userRepository.existsByEmail(nUser.getUserEmail()))
+            throw new EmailAlreadyExistsException("email already exists in database");
+
+        nUser.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        return userRepository.save(nUser);
     }
 
     @Override
-    public User findOne(String userName) {
-        return null;
+    public User findOne(String username) {
+        return userRepository.findByUserName(username);
     }
 }
